26f0d905f8b8be6d78d428c8f4f8881fd6c0aeee
Robin Hahling <robin.hahling@gw-computing.net>
2013-06-16 18:18:30 +0200

    created a method that parses a json and return a collection of tweets

---
+++ src/utils.py
@@ -0,0 +1,25 @@
+import tweet as t
+
+import json
+
+def json_to_tweets(json_tweet_file):
+    """Map each json object to a Twitter object, returning a collection of
+    tweets"""
+
+    tweet_collection = []
+
+    with open(json_tweet_file) as f:
+        data = json.load(f)
+
+    for tw in data['statuses']:
+        tweet = t.Tweet(tw['id'],
+                        tw['user']['id'],
+                        tw['created_at'],
+                        tw['text'],
+                        tw['in_reply_to_status_id'],
+                        tw['in_reply_to_user_id'])
+        tweet_collection.append(tweet)
+
+    return tweet_collection
+
+
