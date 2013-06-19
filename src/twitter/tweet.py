7336f99b70bf60d109bb130bf92e42c082b258f8
Robin Hahling <robin.hahling@gw-computing.net>
2013-06-19 17:16:27 +0200

    added entities to tweet attributes

--- src/twitter/tweet.py
+++ src/twitter/tweet.py
@@ -8,12 +8,14 @@ class Tweet:
                  user_id,
                  created_at,
                  text,
+                 entities,
                  in_reply_to_status_id,
                  in_reply_to_user_id):
         self.id = id
         self.user = u.User(user_id)
         self.created_at = created_at
         self.text = text
+        self.entities = entities
         self.in_reply_to_status_id = in_reply_to_status_id
         self.in_reply_to_user_id = in_reply_to_user_id
 
@@ -23,11 +25,13 @@ class Tweet:
                 "user=%s,"
                 "created_at=%s,"
                 "text=%s,"
+                "entities=%s,"
                 "in_reply_to_status_id=%s,"
                 "in_reply_to_user_id=%s>") % (self.id,
                                               str(self.user),
                                               self.created_at,
                                               self.text,
+                                              self.entities,
                                               self.in_reply_to_status_id,
                                               self.in_reply_to_user_id)
 
@@ -37,10 +41,12 @@ class Tweet:
                 "user: %s\n"
                 "created_at: %s\n"
                 "text: %s\n"
+                "entities: %s\n"
                 "in_reply_to_status_id: %s\n"
                 "in_reply_to_user_id: %s\n") % (self.id,
                                                 str(self.user),
                                                 self.created_at,
                                                 self.text,
+                                                self.entities,
                                                 self.in_reply_to_status_id,
                                                 self.in_reply_to_user_id)
