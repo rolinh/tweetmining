d80553c9992296c40adf473eb4571e9ab5a3056c
Robin Hahling <robin.hahling@gw-computing.net>
2013-06-19 15:17:36 +0200

    added naive bayes classifier prototype

---
+++ src/classifiers/naive_bayes.py
@@ -0,0 +1,14 @@
+from classifiers import abstract_classifier
+
+from nltk import
+
+class NaiveBayes(AbstractClassifier):
+
+#FIXME implement this method
+    def train(self, trainset):
+        pass
+
+#FIXME implement this method
+    def test(self, testset):
+        pass
+
