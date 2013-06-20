e2f3cf7b2294f45f3a24618f92fd3710ac7bac4a
Robin Hahling <robin.hahling@gw-computing.net>
2013-06-20 18:53:19 +0200

    updated script to fetch tweets

--- scripts/fetch-devset.py
+++ scripts/fetch-devset.py
@@ -4,8 +4,12 @@
 import credentials as c
 
 import json
+import codecs
+import sys
 
 from twython import Twython, TwythonError
+
+min_id = 1000000000000000000000000000000000000000000000000000000000000000000000
 
 # Twitter API >= v1.1 requires an authentification... even to fetch public data!
 twitter = Twython(c.APP_CONSUMER_KEY,
@@ -13,11 +17,24 @@ twitter = Twython(c.APP_CONSUMER_KEY,
                   c.OAUTH_TOKEN,
                   c.OAUTH_TOKEN_SECRET)
 
-try:
-    result = twitter.search(q='shampoo', count=100, lang='en')
-except TwythonError as e:
-    print(e)
+fp = codecs.open('../data/devset.json', 'a', 'utf-8')
+for i in range(720):
+    try:
+        result = twitter.search(q='shampoo',
+                                count=100,
+                                lang='en',
+                                max_id=min_id)
+    except TwythonError as e:
+        print(e)
+        sys.exit(1)
 
-with open('../data/devset.json', 'w') as fp:
+    for tw in result['statuses']:
+            id = tw['id']
+            if id < min_id:
+                min_id = id
+
+    fp.write('\n')
     json.dump(result, fp)
 
+fp.close()
+sys.exit(0)
