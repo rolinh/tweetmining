ab9ed7dea9563d151bb5d9840f01387f6fcc87f3
Robin Hahling <robin.hahling@gw-computing.net>
2013-06-21 21:00:00 +0200

    renamed favorite_feature => favorite_count_feature

---
+++ src/features/favorite_count_feature.py
@@ -0,0 +1,13 @@
+from features import abstract_feature as af
+
+class FavoriteCountFeature(af.AbstractFeature):
+
+    def __repr__(self):
+        return "<FavoriteCountFeature>"
+
+    def __str__(self):
+        return "Favorite Count Feature"
+
+    def extract(self, tweet):
+        return "favorite_count_feature", tweet.favorite_count
+
