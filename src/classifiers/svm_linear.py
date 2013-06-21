41f0078a78a85fddf0ddb76e2b2da60e07c0d43e
Robin Hahling <robin.hahling@gw-computing.net>
2013-06-21 23:34:01 +0200

    added svm classifier with linear kernel

---
+++ src/classifiers/svm_linear.py
@@ -0,0 +1,13 @@
+from classifiers import abstract_svm as asvm
+
+class SVMLinear(asvm.AbstractSVM):
+
+    def __repr__(self):
+        return "<SVMLinear>"
+
+    def __str__(self):
+        return "Support Vector Machine with linear kernel"
+
+    def train(self, labels, train_set):
+        super(SVMRBF, self).train_helper(labels, train_set, 'linear')
+
