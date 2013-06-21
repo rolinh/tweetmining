dc60c6e555ab242d1043941e078e449aae1750a8
Robin Hahling <robin.hahling@gw-computing.net>
2013-06-21 18:19:14 +0200

    added hashtag count feature

---
+++ src/features/hashtag_count_feature.py
@@ -0,0 +1,12 @@
+from features import abstract_feature as af
+
+class HashtagCountFeature(af.AbstractFeature):
+
+    def __repr__(self):
+        return "<HashtagCountFeature>"
+
+    def __str__(self):
+        return "Hashtag Count Feature"
+
+    def extract(self, tweet):
+        return "hashtag_count_feature", len(tweet.entities.hastags)
