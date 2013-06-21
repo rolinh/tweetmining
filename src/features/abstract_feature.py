98b329ecc970f9210385f7c92cf0ac5407e092bf
Robin Hahling <robin.hahling@gw-computing.net>
2013-06-21 17:30:35 +0200

    fixed features abstract class so it uses ABCMeta

--- src/features/abstract_feature.py
+++ src/features/abstract_feature.py
@@ -1,12 +1,22 @@
+from abc import ABCMeta, abstractmethod
+
 class AbstractFeature:
+    """Abstract class wrapper for a feature."""
+    __metaclass__ = ABCMeta
 
+    def __init__(self, data=None):
+        self.data = data
+
+    @abstractmethod
     def __repr__(self):
-        raise NotImplementedError("Please Implement this method")
+        pass
 
+    @abstractmethod
     def __str__(self):
-        raise NotImplementedError("Please Implement this method")
+        pass
 
+    @abstractmethod
     def extract(self, tweet):
         """Extract the features and return 2 values: key, aka the feature name
         and value aka the extracted feature value."""
-        raise NotImplementedError("Please Implement this method")
+        pass
