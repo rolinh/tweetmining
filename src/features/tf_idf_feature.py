c455327adfc851cc3d392837ca28806a5bf155e6
Robin Hahling <robin.hahling@gw-computing.net>
2013-06-22 17:22:43 +0200

    added tf-idf feature

---
+++ src/features/tf_idf_feature.py
@@ -0,0 +1,23 @@
+from features import abstract_feature as af
+import words_processing as wp
+import nltk
+
+class TfIdf(af.AbstractFeature):
+
+    def __repr__(self):
+        return "<TfIdf>"
+
+    def __str__(self):
+        return "TF-IDF"
+
+    def extract(self, tweet):
+        freq_max = 0
+        text = wp.preprocessing(tweet.text)
+        tokens = nltk.word_tokenize(text)
+
+        for word in tokens:
+            v = self.data[word]
+            if v > freq_max:
+                freq_max = v
+
+        return "tf_idf",v
